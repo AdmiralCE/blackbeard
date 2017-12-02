499c4ef5c45cdb5c06df70939d9e428ece32e13d
Taylor Krusen <taykrusen@gmail.com>
2017-12-02 14:43:51 -0800

    getNumPages working for ADA site

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ async function getPage() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto('http://www.ada.org/en/ccepr/find-ce-courses#sort=relevancy');
+    await page.goto('http://www.ada.org/en/ccepr/find-ce-courses#sort=relevancy', {waitUntil: 'networkidle2'});
     // await page.screenshot({ path: 'screenshots/ada.png', fullPage: true })
     let listings = await getNumPages(page);
     browser.close();
@@ -12,33 +12,18 @@ async function getPage() {
 
 async function getNumPages(page) {
 
-    const test_select = '#form1 > header > div > div > div > a > img';
     const LISTINGS_SELECTOR = '#search > div.coveo-results-section > div:nth-child(4) > div.coveo-results-header > div.coveo-summary-section > span.CoveoQuerySummary > span > span:nth-child(3)';
 
-    const testStuff = await page.$eval(LISTINGS_SELECTOR, el => el.innerText());
+    const inner = await page.$eval(LISTINGS_SELECTOR, el => {
+        let html = el.textContent
+        return html.replace(',','').trim();
+    });
 
-    // const theText = await testStuff.getProperty
-    console.log(testStuff)
-    // const html = await page.evaluate(stuff => stuff.innerHTML, testStuff);
-    // console.log(html);
-
-    // await testStuff.dispose();
-    // let bacon = page.$(test_select);
-    // console.log(bacon);
-    // const inner = await page.evaluate(sel => {
-    //     const html = page.$(sel);
-
-    //     return HTML;
-    // }, LISTINGS_SELECTOR);
-
-    // console.log(numListings);
-//   console.log('numUsers: ', numUsers);
-
-//   /*
-//   * GitHub shows 10 resuls per page, so
-//   */
-//   let numPages = Math.ceil(numUsers / 10);
-//   return numPages;
+    const numListings = parseInt(inner);
+    // the ADA site allows 10 results per page, so
+    const numPages = Math.ceil(numListings / 10); 
+    console.log("ADA had "+ numListings + " listings over " + numPages + " pages.")
+    return numPages;
 }
 
 getPage();
